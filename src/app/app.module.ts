e0f3cc837ad8745a67e843d3ebe04e178fc0fab8
Rajkrithika <mail2krithika1@gmail.com>
2025-09-18 10:24:50 -0400

    Added Simple UI and Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeModule } from './home/home.module';
+import { ReservationModule } from './reservation/reservation.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HomeModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReservationModule
   ],
   providers: [
     provideClientHydration()
