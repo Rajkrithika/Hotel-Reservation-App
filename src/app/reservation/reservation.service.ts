31a731e10725ebac694533c5e92266cc7bf44d42
Rajkrithika <mail2krithika1@gmail.com>
2025-09-23 20:35:42 -0400

    ng template and local reference

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,35 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+  private reservations: Reservation[] = [];
+  private isBrowser: boolean;
 
-  private reservations : Reservation[] = [];
-  
-  constructor() {
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
-   }
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+
+    if (this.isBrowser) {
+      const savedReservations = localStorage.getItem("reservations");
+      this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+    }
+  }
 
   getReservations(): Reservation[] {
     return this.reservations;
   }
-  getReservation(id: number) : Reservation | undefined {
-    return this.reservations.find(res=> res.id === id);
+
+  getReservation(id: number): Reservation | undefined {
+    return this.reservations.find(res => res.id === id);
   }
 
-  addReservation(reservation: Reservation) : void{
+  addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
     console.log(this.reservations);
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
-  } 
 
-  deleteReservation (id:number) : void {
-    this.reservations = this.reservations.filter(res=> res.id !== id);
-    this.reservations.splice(id, 1);
-        localStorage.setItem('reservations', JSON.stringify(this.reservations));
+    if (this.isBrowser) {
+      localStorage.setItem('reservations', JSON.stringify(this.reservations));
+    }
+  }
 
+  deleteReservation(id: number): void {
+    this.reservations = this.reservations.filter(res => res.id !== id);
+
+    if (this.isBrowser) {
+      localStorage.setItem('reservations', JSON.stringify(this.reservations));
+    }
   }
 }
